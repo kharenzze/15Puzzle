d1842709c16de06b2a92ec0775a21c979d2118a1
Paolo <str237@gmail.com>
2016-10-31 01:34:09 +0100

    Added Memory class

---
+++ src/puzzle/Memory.java
@@ -0,0 +1,29 @@
+package puzzle;
+
+import java.util.HashMap;
+
+/**
+ * Created by Paolo on 25/10/16.
+ */
+public class Memory {
+    HashMap<Integer, Boolean> table;
+    long count;
+
+    public Memory() {
+        this.table = new HashMap<Integer,Boolean>();
+        this.count = 0;
+    }
+
+    public boolean check(NPuzzle puzzle){
+        int hash =puzzle.tablero.hashCode();
+
+        boolean status = this.table.getOrDefault(hash, false);
+        if (status){
+            return true;
+        }else{
+            this.table.put(hash,true);
+            count++;
+            return false;
+        }
+    }
+}
